0be401959c7088874ead3cf196238bc7b95706b3
Idan <you@example.com>
2021-07-12 14:53:46 +0300

    initial commit of react client and signalr webapi server, started building hub methods

---
+++ chatServer/chatServer/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using chatServer.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinRoom(string username,string roomNumber)
+        {
+            await Groups.AddToGroupAsync(username, roomNumber);
+        }
+        public async Task LeaveRoom(string username, string roomNumber)
+        {
+            await Groups.RemoveFromGroupAsync(username, roomNumber);
+        }
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ chatServer/chatServer/Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chatServer.Models
+{
+    public class ChatMessage
+    {
+    }
+}
