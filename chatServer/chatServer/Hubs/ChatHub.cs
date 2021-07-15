dd66bcc5005ad3b8e86252dcc6eb04736ee69a08
Idan <you@example.com>
2021-07-15 16:39:21 +0300

    updated backed with proper classes and methods for the chat. keeping data in a static list instead of DB

--- chatServer/chatServer/Hubs/ChatHub.cs
+++ chatServer/chatServer/Hubs/ChatHub.cs
@@ -9,31 +9,63 @@ namespace chatServer.Hubs
 {
     public class ChatHub : Hub
     {
-        List<Room> rooms = new List<Room> {
+       static List<Room> rooms = new List<Room> {
         new Room { roomName = "General" },
         new Room { roomName = "Random" },
         new Room { roomName = "Sports" }
         };
         
-        public async Task JoinRoom(string username,string roomName)
+        public async Task ChangeRoom(string username,string joinedRoomName,string leftRoomName )
         {
-            await Groups.AddToGroupAsync(username, roomName);
-            var room = rooms.Find((x) => x.roomName == roomName);
-            room.userList.Add(username);
-            await Clients.Caller.SendAsync("JoinRoom", room.messageList,room.userList);
-            await Clients.OthersInGroup(roomName).SendAsync("RecieveMessage", "User " + username + " joined the room!");
+            try
+            {
+                var room = rooms.Find((x) => x.roomName == joinedRoomName);
+                if (!String.IsNullOrEmpty(leftRoomName))
+                {
+                    var leftRoom = rooms.Find((x) => x.roomName == leftRoomName);
+                    leftRoom.userList.Remove(username);
+                    await Groups.RemoveFromGroupAsync(Context.ConnectionId, leftRoomName);
+                }
+                    await Groups.AddToGroupAsync(Context.ConnectionId, joinedRoomName);
+                    room.userList.Add(username);
+                    await Clients.OthersInGroup(joinedRoomName).SendAsync("ReceiveMessage", "User " + username + " joined the room!", "Admin");
+                    await Clients.Caller.SendAsync("JoinRoom", room.messageList, room.userList);
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+                
         }
-        public async Task LeaveRoom(string username, string roomName)
+        public  Task LeaveRoom(string username, string roomName)
         {
-            var room = rooms.Find((x) => x.roomName == roomName);
-            room.userList.Remove(username);
-            await Groups.RemoveFromGroupAsync(username, roomName);
+            try
+            {
+                var room = rooms.Find((x) => x.roomName == roomName);
+                room.userList.Remove(username);
+                return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
         public async Task SendMessage(string message,string roomName,string username)
         {
-            var room = rooms.Find((x) => x.roomName == roomName);
-            room.messageList.Add(new ChatMessage { username = username, message = message });
-            await Clients.Group(roomName).SendAsync("RecieveMessage", username,message);
+            try
+            {
+                var room = rooms.Find((x) => x.roomName == roomName);
+                room.messageList.Add(new ChatMessage { username = username, message = message });
+                await Clients.Group(roomName).SendAsync("ReceiveMessage", message, username);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+          
         }
     }
 }
--- chatServer/chatServer/Models/Room.cs
+++ chatServer/chatServer/Models/Room.cs
@@ -8,7 +8,7 @@ namespace chatServer.Models
     public class Room
     {
         public string roomName { get; set; }
-        public List<ChatMessage> messageList { get; set; }
-        public List<string> userList { get; set; }
+        public List<ChatMessage> messageList { get; set; } = new List<ChatMessage>();
+        public List<string> userList { get; set; } = new List<string>();
     }
 }
